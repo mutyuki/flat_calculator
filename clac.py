9e371fc3a6ac51bfae442e3018cc847bb42e43ed
mutyuki <belandinging01@gmail.com>
2024-12-03 08:49:55 +0900

    win

---
+++ clac.py
@@ -0,0 +1,62 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Calc App"
+    result = ft.Text(value="0")
+
+    page.add(
+        ft.Row(controls=[result]),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="AC"),
+                ft.ElevatedButton(text="+/-"),
+                ft.ElevatedButton(text="%"),
+                ft.ElevatedButton(text="/"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="7"),
+                ft.ElevatedButton(text="8"),
+                ft.ElevatedButton(text="9"),
+                ft.ElevatedButton(text="*"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="4"),
+                ft.ElevatedButton(text="5"),
+                ft.ElevatedButton(text="6"),
+                ft.ElevatedButton(text="-"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="1"),
+                ft.ElevatedButton(text="2"),
+                ft.ElevatedButton(text="3"),
+                ft.ElevatedButton(text="+"),
+            ]
+        ),
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="0"),
+                ft.ElevatedButton(text="."),
+                ft.ElevatedButton(text="="),
+            ]
+        ),
+        ft.Container(
+            width=350,
+            bgcolor=ft.Colors.BLACK,
+            border_radius=ft.border_radius.all(20),
+            padding=20,
+            content=ft.Column(
+                controls=[]  # Controls will the six rows with the text
+                # and the calculator buttons.
+            ),
+        ),
+    )
+
+
+ft.app(main)
